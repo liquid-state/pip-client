143dec70745fac8c61eb99bd619bf71c4225563b
John Briggs <john@liquid-state.com>
2020-05-20 21:52:59 +1000

    getAppUser

--- src/admin-client.ts
+++ src/admin-client.ts
@@ -20,12 +20,20 @@ const domainMap: { [key: string]: string } = {
   objectTypes: '/api/v1/object_types/',
   acceptables: '/api/v1/acceptables/',
   acceptances: '/api/v1/acceptances/',
+  users: '/api/admin/v1/users/',
 };
 
 export default class PIPAdminClient {
   private options: IOptions;
   constructor(private identity: IdentityOptions, options?: IOptions) {
     this.options = options ? options : defaultOptions;
+  }
+
+  getAppUser = async (appUserUISId: string): Promise<object> => {
+    const url = `${this.getUrl('users')}${appUserUISId}`;
+    const resp = await fetch(url, { headers: this.headers() });
+    this.verifyResponse(resp);
+    return resp.json();
   }
 
   listObjectTypes = async (): Promise<ObjectType[]> => {
